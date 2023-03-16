9af47fa5b1b309fc1db3d51a4b9aeac800aa1171
Scout Van den Bergh <scoutvandenbergh@gmail.com>
2023-03-16 17:01:23 +0100

    implemented transformers

--- evalpgm/models.py
+++ evalpgm/models.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 import math
-
+from rotary_embedding_torch import RotaryEmbedding
 
 class Permute(nn.Module): 
     def __init__(self, *args):
@@ -87,19 +87,10 @@ class VAE(ModelBackbone):
         bottleneck_size = 128, 
         learning_rate = 0.0001, 
         blocks_per_stage = 4,
+        n_heads = 8,
         n_warmup_steps = 1000,
         beta = 0.001):
-        """
-        This would be my prefered VAE implementation
-        Instead of max poolings it uses strided convs
-        A strided conv where stride = kernel size, functions the same way max pooling does,
-        but then learns filters for the pooling instead of max
 
-        Also for the decoder there is convtranspose1d instead of upsample.
-
-        Take not of how deep this model is with how few parameters. (1.3 mill)
-        This is the power of aggresive pooling (through strided convs)
-        """
         super().__init__(learning_rate = learning_rate, n_warmup_steps = n_warmup_steps)
         self.save_hyperparameters()
         
@@ -107,15 +98,15 @@ class VAE(ModelBackbone):
         self.encoder = nn.Sequential(
             nn.Embedding(vocab_size, hidden_sizes[0]), # gives B x 1024 x 128
             Permute(0,2,1), # B x 128 x 1024
-            *[ResidualBlock(hidden_sizes[0], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.Conv1d(hidden_sizes[0], hidden_sizes[1], 4, stride = 4), # 1024 -> 256
-            *[ResidualBlock(hidden_sizes[1], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.Conv1d(hidden_sizes[1], hidden_sizes[2], 4, stride = 4), # 256 -> 64
-            *[ResidualBlock(hidden_sizes[2], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.Conv1d(hidden_sizes[2], hidden_sizes[3], 4, stride = 4), # 64 -> 16
-            *[ResidualBlock(hidden_sizes[3], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.Conv1d(hidden_sizes[3], hidden_sizes[4], 4, stride = 4), # 16 -> 4
-            *[ResidualBlock(hidden_sizes[4], 5, 0.2) for _ in range(blocks_per_stage)],
+            *[ResidualBlock(hidden_sizes[0], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.Conv1d(hidden_sizes[0], hidden_sizes[1], kernel_size = 4, stride = 4), # 1024 -> 256
+            *[ResidualBlock(hidden_sizes[1], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.Conv1d(hidden_sizes[1], hidden_sizes[2], kernel_size = 4, stride = 4), # 256 -> 64
+            *[ResidualBlock(hidden_sizes[2], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.Conv1d(hidden_sizes[2], hidden_sizes[3], kernel_size = 4, stride = 4), # 64 -> 16
+            *[ResidualBlock(hidden_sizes[3], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.Conv1d(hidden_sizes[3], hidden_sizes[4], kernel_size = 4, stride = 4), # 16 -> 4
+            *[ResidualBlock(hidden_sizes[4], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
             View(-1, hidden_sizes[4]*4),
             nn.Linear(hidden_sizes[4]*4, bottleneck_size),
             nn.GELU()
@@ -129,29 +120,29 @@ class VAE(ModelBackbone):
             nn.Linear(bottleneck_size, hidden_sizes[4]*4),
             nn.GELU(),
             View(-1, hidden_sizes[4], 4), # put them into B x 256 x 4, just as it was in last conv of encoder
-            *[ResidualBlock(hidden_sizes[4], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.ConvTranspose1d(hidden_sizes[4], hidden_sizes[3], 4, stride = 4), # 4 -> 16
-            *[ResidualBlock(hidden_sizes[3], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.ConvTranspose1d(hidden_sizes[3], hidden_sizes[2], 4, stride = 4), # 4 -> 16
-            *[ResidualBlock(hidden_sizes[2], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.ConvTranspose1d(hidden_sizes[2], hidden_sizes[1], 4, stride = 4), # 4 -> 16
-            *[ResidualBlock(hidden_sizes[1], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.ConvTranspose1d(hidden_sizes[1], hidden_sizes[0], 4, stride = 4), # 4 -> 16
-            *[ResidualBlock(hidden_sizes[0], 5, 0.2) for _ in range(blocks_per_stage)],
-            nn.Conv1d(hidden_sizes[0], vocab_size, 1), # to output
+            *[ResidualBlock(hidden_sizes[4], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.ConvTranspose1d(hidden_sizes[4], hidden_sizes[3], kernel_size = 4, stride = 4), # 4 -> 16
+            *[ResidualBlock(hidden_sizes[3], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.ConvTranspose1d(hidden_sizes[3], hidden_sizes[2], kernel_size = 4, stride = 4), # 4 -> 16
+            *[ResidualBlock(hidden_sizes[2], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.ConvTranspose1d(hidden_sizes[2], hidden_sizes[1], kernel_size = 4, stride = 4), # 4 -> 16
+            *[ResidualBlock(hidden_sizes[1], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.ConvTranspose1d(hidden_sizes[1], hidden_sizes[0], kernel_size = 4, stride = 4), # 4 -> 16
+            *[ResidualBlock(hidden_sizes[0], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            nn.Conv1d(hidden_sizes[0], vocab_size, kernel_size = 1), # to output
         )
 
-        self.decoderL = nn.Sequential(
-            nn.Linear(bottleneck_size, 64),
-            nn.GELU(),
-            nn.Linear(64,32),
-            nn.GELU(),
-            nn.Linear(32,16),
-            nn.GELU(),
-            nn.Linear(16,4),
-            nn.GELU(),
-            nn.Linear(4,1)
-        )
+        # self.decoderL = nn.Sequential(
+        #     nn.Linear(bottleneck_size, 64),
+        #     nn.GELU(),
+        #     nn.Linear(64,32),
+        #     nn.GELU(),
+        #     nn.Linear(32,16),
+        #     nn.GELU(),
+        #     nn.Linear(16,4),
+        #     nn.GELU(),
+        #     nn.Linear(4,1)
+        # )
 
         self.beta = beta
 
@@ -162,8 +153,8 @@ class VAE(ModelBackbone):
     def decode(self, x):
         return self.decoder(x)
     
-    def decodeL(self, x):
-        return self.decodeL(x) #added to predict length of protein without padding
+    # def decodeL(self, x):
+    #     return self.decodeL(x) #added to predict length of protein without padding
 
     def reparameterize(self, mean, logvar):
         std = torch.exp(0.5*logvar)
@@ -173,7 +164,145 @@ class VAE(ModelBackbone):
     def forward(self,x):
         mean, logvar = self.encode(x)
         z = self.reparameterize(mean, logvar)
-        return self.decode(z), mean, logvar, self.decodeL(z) #added decodeL to forward pass
+        return self.decode(z), mean, logvar #, self.decodeL(z) #added decodeL to forward pass
+        
+    def training_step(self, batch, batch_idx):
+        X, _ = batch
+        recon_x, mean, log_var = self.forward(X)
+        reconstruction_loss = F.cross_entropy(recon_x, X)
+        KL_divergence = -0.5 * torch.sum(1 + log_var - mean**2 - torch.exp(log_var))
+
+        loss = reconstruction_loss + self.beta * KL_divergence #implement loss for correct length
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx): 
+        X, _ = batch
+        recon_x, mean, log_var = self.forward(X)
+        reconstruction_loss = F.cross_entropy(recon_x, X)
+        KL_divergence = -0.5 * torch.sum(1 + log_var - mean**2 - torch.exp(log_var))
+
+        loss = reconstruction_loss + self.beta * KL_divergence #implement loss for correct length
+        self.log('val_loss', loss)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        X, _ = batch
+        recon_x, mean, log_var = self.forward(X)
+        reconstruction_loss = F.cross_entropy(recon_x, X)
+        KL_divergence = -0.5 * torch.sum(1 + log_var - mean**2 - torch.exp(log_var))
+
+        loss = reconstruction_loss + self.beta * KL_divergence #implement loss for correct length
+        self.log('test_loss', loss)
+        return loss
+
+class VAE_transformer(ModelBackbone):
+    def __init__(
+        self,
+        vocab_size = 21, 
+        hidden_sizes = [64, 128, 256, 512, 1024], 
+        bottleneck_size = 128, 
+        learning_rate = 0.0001, 
+        blocks_per_stage = 4,
+        n_heads = 8,
+        n_warmup_steps = 1000,
+        beta = 0.001):
+
+        super().__init__(learning_rate = learning_rate, n_warmup_steps = n_warmup_steps)
+        self.save_hyperparameters()
+        
+
+        self.encoder = nn.Sequential(
+            nn.Embedding(vocab_size, hidden_sizes[0]), # gives B x 1024 x 128
+            Permute(0,2,1), # B x 128 x 1024
+            #*[ResidualBlock(hidden_sizes[0], kernel_size= 5, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[0], hidden_sizes[0], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.Conv1d(hidden_sizes[0], hidden_sizes[1], kernel_size = 4, stride = 4), # 1024 -> 256
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[1], hidden_sizes[1], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.Conv1d(hidden_sizes[1], hidden_sizes[2], kernel_size = 4, stride = 4), # 256 -> 64
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[2], hidden_sizes[2], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.Conv1d(hidden_sizes[2], hidden_sizes[3], kernel_size = 4, stride = 4), # 64 -> 16
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[3], hidden_sizes[3], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.Conv1d(hidden_sizes[3], hidden_sizes[4], kernel_size = 4, stride = 4), # 16 -> 4
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[4], hidden_sizes[4], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            View(-1, hidden_sizes[4]*4),
+            nn.Linear(hidden_sizes[4]*4, bottleneck_size),
+            nn.GELU()
+        )
+        
+        self.lin_mu = nn.Linear(bottleneck_size, bottleneck_size)
+        self.lin_var = nn.Linear(bottleneck_size, bottleneck_size)
+
+        # note that I use quite mirrored encoder and decoder here
+        self.decoder = nn.Sequential(
+            nn.Linear(bottleneck_size, hidden_sizes[4]*4),
+            nn.GELU(),
+            View(-1, hidden_sizes[4], 4), # put them into B x 256 x 4, just as it was in last conv of encoder
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[4], hidden_sizes[4], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.ConvTranspose1d(hidden_sizes[4], hidden_sizes[3], kernel_size = 4, stride = 4), # 4 -> 16
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[3], hidden_sizes[3], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.ConvTranspose1d(hidden_sizes[3], hidden_sizes[2], kernel_size = 4, stride = 4), # 4 -> 16
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[2], hidden_sizes[2], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.ConvTranspose1d(hidden_sizes[2], hidden_sizes[1], kernel_size = 4, stride = 4), # 4 -> 16
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[1], hidden_sizes[1], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.ConvTranspose1d(hidden_sizes[1], hidden_sizes[0], kernel_size = 4, stride = 4), # 4 -> 16
+            Permute(0,2,1),
+            *[TransformerRoPE(hidden_sizes[0], hidden_sizes[0], n_heads, dropout = 0.2) for _ in range(blocks_per_stage)],
+            Permute(0,2,1),
+            nn.Conv1d(hidden_sizes[0], vocab_size, kernel_size = 1), # to output
+        )
+
+        # self.decoderL = nn.Sequential(
+        #     nn.Linear(bottleneck_size, 64),
+        #     nn.GELU(),
+        #     nn.Linear(64,32),
+        #     nn.GELU(),
+        #     nn.Linear(32,16),
+        #     nn.GELU(),
+        #     nn.Linear(16,4),
+        #     nn.GELU(),
+        #     nn.Linear(4,1)
+        # )
+
+        self.beta = beta
+
+    def encode(self, x):
+        x = self.encoder(x)
+        return self.lin_mu(x), self.lin_var(x)
+    
+    def decode(self, x):
+        return self.decoder(x)
+    
+    # def decodeL(self, x):
+    #     return self.decodeL(x) #added to predict length of protein without padding
+
+    def reparameterize(self, mean, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mean + eps*std
+    
+    def forward(self,x):
+        mean, logvar = self.encode(x)
+        z = self.reparameterize(mean, logvar)
+        return self.decode(z), mean, logvar #, self.decodeL(z) #added decodeL to forward pass
         
     def training_step(self, batch, batch_idx):
         X, _ = batch
@@ -234,8 +363,63 @@ class MultiHeadAttentionLayer(nn.Module):
         
         attention = attention.transpose(1, 2).contiguous().view(B, L, C) # (B, L, nh, hs)
         attention = self.output(attention)
+        print(attention.shape)
         
         return attention
+
+class RoPEMultiHeadAttentionLayer(nn.Module):
+    def __init__(self, hidden_size, n_heads, dropout): #add argument to choose for rotary or abs, or relative, or learned
+        super().__init__()
+        
+        assert hidden_size % n_heads == 0
+        
+        self.hidden_size = hidden_size
+        self.n_heads = n_heads
+        self.head_dim = hidden_size // n_heads
+        
+        self.kqv = nn.Linear(hidden_size, hidden_size * 3, bias=False) 
+        self.output = nn.Linear(hidden_size, hidden_size)
+        
+        self.dropout = nn.Dropout(dropout)
+        self.rotary_emb = RotaryEmbedding(dim=self.head_dim)
+        
+    def forward(self, x):
+        B, L, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        k, q, v = self.kqv(x).chunk(3, dim=-1)
+        
+        k = k.view(B, L, self.n_heads, self.head_dim).transpose(1, 2) # (B, nh, L, hs)
+        q = q.view(B, L, self.n_heads, self.head_dim).transpose(1, 2) # (B, nh, L, hs)
+        v = v.view(B, L, self.n_heads, self.head_dim).transpose(1, 2) # (B, nh, L, hs)
+        
+        q = self.rotary_emb.rotate_queries_or_keys(q)
+        k = self.rotary_emb.rotate_queries_or_keys(k)
+
+        attn = torch.matmul(q, k.transpose(-2,-1)) * k.size(-1)**0.5 #(B, nh, L, hs) x (B, nh, hs, L) -> (B, nh, L, L)
+        attention = F.softmax(attn, dim=-1)
+        attention = torch.matmul(self.dropout(attention), v) #(B, nh, L, hs)
+        
+        attention = attention.transpose(1, 2).contiguous().view(B, L, C) # (B, L, nh, hs)
+        attention = self.output(attention)
+        
+        return attention
+    
+class TransformerRoPE(nn.Module):
+    def __init__(self, input_dim, hidden_size, n_heads, dropout):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(input_dim)
+        self.ln2 = nn.LayerNorm(input_dim)
+        self.RoPE_MHA = RoPEMultiHeadAttentionLayer(hidden_size, n_heads, dropout)
+        self.ffn = SwiGLU(input_dim)
+        
+    def forward(self, x):
+        pre_ln = self.ln1(x)
+        attn = self.RoPE_MHA(pre_ln)
+        residual = attn + x
+
+        pre_ln = self.ln2(residual)
+        ffn = self.ffn(pre_ln)
+        output = ffn + residual
+        return output
     
 class GLU(nn.Module):
     def __init__(self, input_dim):
@@ -258,13 +442,13 @@ class SwiGLU(nn.Module):
     def forward(self, x):
         gates = self.linear(x).chunk(2, dim=-1) #split into 2 parts
         #Swish_beta(xW+b) * (xV+c)
-        activation = (gates[0] * F.sigmoid(self.beta * gates[1])) * gates[1]
+        activation = (gates[0] * torch.sigmoid(self.beta * gates[1])) * gates[1]
         return activation
 
 class GeGLU(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim):
         super().__init__()
-        self.linear = nn.Linear(input_dim, output_dim * 2)
+        self.linear = nn.Linear(input_dim, input_dim * 2)
 
     def forward(self, x):
         gates = self.linear(x).chunk(2, dim=-1) #split into 2 parts
