fixed length decoder, fixed numerical issues in generate.py etc

evalpgm/FED/process_embeddings.py
@@ -43,9 +43,9 @@ def process_embeddings_ESM2_35M(model, data, lengths):
     sequence_representations = []
     counter = 0
 
-    for i, batch in enumerate(tqdm(dataloader)):
+    for i, batch in enumerate(tqdm(dataloader, mininterval=60)): #show tqdm progress every 60 seconds.
         if i % 250 == 0:
-            print(f"Step{i}")
+            print(f"Step{i}", flush=True) #test if this works, without flush it only shows the print staments after all the, now deleted, tqdm output was shown so irrelevant!
 
         with torch.no_grad():
             results = model(batch.to(device), repr_layers=[12], return_contacts=False)

evalpgm/blocks.py
@@ -246,7 +246,7 @@ class SwiGLU(nn.Module):
         self.linear = nn.Linear(input_dim, input_dim * 8)
         if beta_swi == None:
             self.beta = nn.Parameter(torch.ones(input_dim * 4))
-        else: #prob better to change to another if or elif but not important in current implementation
+        else: 
             self.beta = beta_swi
 
     def forward(self, x):

evalpgm/data.py
@@ -19,9 +19,10 @@ idx_to_tok_esm2_150M = {v : k for k,v in tok_to_idx_esm2_150M.items()}
 
 def pad_batch(f, sample, pad_to = 1024):
     sequence = sample["central"]
-    length = sample["0/protein_length"]
+    length = sample["0/protein_length"] 
+    length_normalized = length / 1024 #added, suggestion Gaetan
     sequence = np.concatenate([sequence, np.ones((pad_to - length), dtype=sequence.dtype)])
-    return sequence, length.astype(np.float32)
+    return sequence, length_normalized.astype(np.float32) #modified, originally length.astype ...
     
 
 class Uniref50DataModule(pl.LightningDataModule):

evalpgm/scripts/generate.py
@@ -15,25 +15,73 @@ def generate_sequences(model, amount = 25000, temperature = 0.8):
     start = time.time()
 
     z = torch.randn(amount, 128).cuda()
+    model.eval()
+    roundedLengths = []
 
-    dataloader = torch.utils.data.DataLoader(z, batch_size=batch_size, shuffle=False) #use dataloader to keep VRAM requirements low during inference. 25k at once = > 11k VRAM, 256 at once is about .5GB VRAM
+    dataloader = torch.utils.data.DataLoader(z, batch_size=batch_size, shuffle=False)
 
     # generate new samples
-    for batch in dataloader: #no tqdm to limit amount of unneccessary output
+    for batch in dataloader:
         with torch.no_grad():
-            output = model.decode(batch)
+            output, length_pred = model.decode(batch)
+            lengths = length_pred.squeeze()*1024
+            rounding = [round(num) for num in lengths.tolist()]
+            roundedLengths.extend(rounding)
             softmax = nn.Softmax(dim=-1)
-            output_softmax = softmax(output/temperature).double().cpu().transpose(-2, -1) # transpose required to sample from Categorical, this allows vectorized sampling instead of for loops (seconds instead of hours)
+            output_softmax = softmax(output/temperature).double().cpu().transpose(-2, -1)
 
-            for seq in output_softmax:
+            # Check if the output from softmax sums to 1, if not normalize, fix by GPT4 , required due to numerical issues for certain temperatures
+            if not torch.allclose(output_softmax.sum(-1), torch.tensor(1.0).double(), atol=1e-5):
+                print("Sum of softmax outputs does not equal 1. Normalizing.")
+                output_softmax = output_softmax + 1e-8  # Add a small epsilon
+                output_softmax = output_softmax / output_softmax.sum(-1, keepdim=True)
+
+            for i, seq in enumerate(output_softmax):
                 dist = torch.distributions.Categorical(seq)
                 sampledFromSoftmax = dist.sample().cpu().numpy().tolist()
+                sampledFromSoftmax = sampledFromSoftmax[:roundedLengths[i]] + [1]*(1024-roundedLengths[i])
                 sampledProteins.append(sampledFromSoftmax)
 
     stop = time.time()
 
     print(f"Time elapsed to temperature sample {amount} novel protein sequences:{datetime.timedelta(seconds=stop-start)}")
+    
     sampledProteins_tensor = torch.tensor(sampledProteins)
-    print(sampledProteins_tensor.shape) #otherwise a nested list
+    print(sampledProteins_tensor.shape)
+    return sampledProteins_tensor, roundedLengths, np.mean(roundedLengths)
 
-    return sampledProteins_tensor
+# def generate_sequences(model, amount = 25000, temperature = 0.8):
+#     sampledProteins = []
+#     batch_size = 256
+
+#     start = time.time()
+
+#     z = torch.randn(amount, 128).cuda()
+#     model.eval()
+#     roundedLengths = []
+
+#     dataloader = torch.utils.data.DataLoader(z, batch_size=batch_size, shuffle=False) #use dataloader to keep VRAM requirements low during inference. 25k at once = > 11k VRAM, 256 at once is about .5GB VRAM
+
+#     # generate new samples
+#     for batch in dataloader:
+#         with torch.no_grad():
+#             output, length_pred = model.decode(batch)
+#             lengths = length_pred.squeeze()*1024
+#             rounding = [round(num) for num in lengths.tolist()]
+#             roundedLengths.extend(rounding)
+#             softmax = nn.Softmax(dim=-1)
+#             output_softmax = softmax(output/temperature).double().cpu().transpose(-2, -1) # transpose required to sample from Categorical, this allows vectorized sampling instead of for loops (seconds instead of hours)
+            
+#             for i, seq in enumerate(output_softmax):
+#                 dist = torch.distributions.Categorical(seq)
+#                 sampledFromSoftmax = dist.sample().cpu().numpy().tolist()
+#                 sampledFromSoftmax = sampledFromSoftmax[:roundedLengths[i]] + [1]*(1024-roundedLengths[i])
+#                 sampledProteins.append(sampledFromSoftmax)
+
+#     stop = time.time()
+
+#     print(f"Time elapsed to temperature sample {amount} novel protein sequences:{datetime.timedelta(seconds=stop-start)}")
+#     sampledProteins_tensor = torch.tensor(sampledProteins)
+#     print(sampledProteins_tensor.shape) #otherwise a nested list
+
+#     return sampledProteins_tensor, roundedLengths, np.mean(roundedLengths)